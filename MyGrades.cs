f1e401e952386aed4771d4262da752fe6a849d2a
GeoFelix <felixodhiambo538@gmail.com>
2023-07-07 09:30:49 +0300

    invocked the class at the program.cs

--- MyGrades.cs
+++ MyGrades.cs
@@ -5,6 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GradeCalculator;
-internal class MyGrades
+public class MyGrades
 {
+    public string GetGradesInPercentage(int Percentage)
+    {
+        if (Percentage >= 80 & Percentage <= 100)
+            return "A";
+        else if (Percentage >=60 && Percentage < 80)
+            return "B";
+        else if(Percentage >=50 && Percentage< 60)
+            return "C";
+        else if (Percentage >=40 &&  Percentage <50)
+            return "D";
+        else if (Percentage >0 && Percentage < 40)
+            return "E";
+        else return "Invalid";
+        
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace GradeCalculator;
 
-internal class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //We get the marks that is scored
+        var myGrades = new MyGrades()
+
+;       Console.WriteLine("What is the Marks Scored: ");
+
+        //We convert the inpu into integer
+        var marks= Convert.ToInt32(Console.ReadLine());
+        
+        //Then we take the inserted marks, invoke the getgradeinpercentage class to check
+        //which grade does the inserted marks fall in and store it in a variable called grade.
+        var grade=myGrades.GetGradesInPercentage(marks);
+
+        //Then we output the grade
+        Console.WriteLine($"Your grade is: {grade}");
     }
 }
